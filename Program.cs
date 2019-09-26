2cddb772389385df2030223d5abb6183b5141d32
Anik Hasan <55820217+AnikHasan-web@users.noreply.github.com>
2019-09-26 09:28:40 +0600

    Mthode call

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("goto function anik");
+            Anik();
+            Console.ReadKey();
+
+            
+        }
+        static void Anik()
+        {
+           
+            Console.WriteLine("welcome to my new work about dot net!");
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age" +age);
+            Console.ReadKey();
+
+             
+        }
+    }
+}
